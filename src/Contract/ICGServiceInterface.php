1e55674645570f438ae5908777a5bd5fbab220d5
Amin56413 <Ellie.guo@goldenf.co>
2021-05-04 16:31:48 +0800

    add vendor_code for icg/gpsi

--- src/Contract/ICGServiceInterface.php
+++ src/Contract/ICGServiceInterface.php
@@ -13,18 +13,21 @@ interface ICGServiceInterface {
      * @param string $account
      * @param string $game_code
      * @param string $language
+     * @param string $vendor_code
+     * @param string|null $back_url
      * @return mixed
      */
-    function gameLaunch(string $op_code, string $account, string $game_code, string $language);
+    function gameLaunch(string $op_code, string $account, string $game_code, string $language, string $vendor_code, string $back_url = null);
 
     /**
      * 遊戲詳情
      * @param string $op_code
      * @param string $account
      * @param string $bet_id
+     * @param string $vendor_code
      * @return mixed
      */
-    function gameDetail(string $op_code, string $account, string $bet_id);
+    function gameDetail(string $op_code, string $account, string $bet_id, string $vendor_code);
 
     /**
      * 抓取遊戲紀錄
